587ab2fa036647d9a25705bfcee1c2ac27e9a0cb
Matthias H <matthias.h@gmx.net>
2014-12-08 22:28:27 +0100

    Methoden für ItemHandling im PlayerModel ergänzt
    collectItem um Items aufzusammeln
    activeItems für aktive Items
    useActiveItem um aktives Item zu benutzen
    
    Issue: #8

--- source/model/lab_PlayerModel.js
+++ source/model/lab_PlayerModel.js
@@ -2,13 +2,11 @@ function lab_PlayerModel(){
     
     lab_EntityModel.call(this);
     
-
-    // TODO: health handling
     this.health = 100;
 
     this.inventory = {};
-    
-    
+
+    this.activeItem;
 }
 
 // inherit from lab_EntityModel
@@ -18,11 +16,21 @@ lab_PlayerModel.prototype = Object.create(lab_EntityModel.prototype);
 lab_PlayerModel.prototype.constructor = lab_PlayerModel;
 
 
-lab_PlayerModel.prototype.addToInventory = function(key, value){
-    this.inventory[key] = value;
+lab_PlayerModel.prototype.addToInventory = function(item){
+    this.inventory[item.inventoryPosition] = item;
 };
 
 lab_PlayerModel.prototype.removeFromInventory = function(key){
     delete this.inventory[key];
 };
 
+lab_PlayerModel.prototype.useActiveItem = function(){
+	this.activeItem.use();
+};
+
+lab_PlayerModel.prototype.collectItem = function(item) {
+	// add to inventory
+	this.addToInventory(item);
+	// remove the item from the game world
+	item.removeFromWorld();
+}
