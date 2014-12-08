21c483c7f27d8cad1ca47ac388b73d2ff3af917b
Matthias H <matthias.h@gmx.net>
2014-12-08 20:42:03 +0100

    Workspace Folder entfernt
    Wir sollten mit Branches arbeiten statt mit verschiedenen Ordnern

--- workspace/helper/math.js
+++
@@ -1,5 +0,0 @@
-var randomInt = function(max){
-    return Math.floor(Math.random() * (max+1));
-};
-
-
--- workspace/view/lab_AbstractRenderer.js
+++
@@ -1,7 +0,0 @@
-function lab_AbstractRenderer(width, height){
-    
-    this.init();
-    
-    this.setSize(width, height);
-    
-}
