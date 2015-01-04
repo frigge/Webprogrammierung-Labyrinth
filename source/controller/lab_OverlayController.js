f4c85b3a3e339a4f12425f455e1b1318c3f7b17f
Sascha Fricke <frigge@frg-vfx.com>
2015-01-04 15:22:26 +0100

    added other player attributes

--- source/controller/lab_OverlayController.js
+++ source/controller/lab_OverlayController.js
@@ -2,5 +2,16 @@ lab_OverlayController = function(){ }
 
 lab_OverlayController.prototype.update = function() {
     health = document.getElementById("health");
-    health.innerHTML = gameController.gameModel.player.health + " %";
+    activeitem = document.getElementById("activeitem");
+    inventory = document.getElementById("inventory");
+    passiveitem = document.getElementById("passiveitem");
+
+    player = gameController.gameModel.player;
+    var text = "";
+    text += "Health: " + player.health + " %<br>";
+    text += "Active Item: " + player.activeItem + "<br>";
+    text += "Passive Item: " + player.passiveItem + "<br>";
+    text += "Inventory: " + (player.inventory.length > 0 ? player.inventory : "Empty") + "<br>";
+
+    health.innerHTML = text;
 }
