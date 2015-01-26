ee0ea4413c92d0fbbb1a450709fba91def0a7bd4
Matthias H <matthias.h@gmx.net>
2015-01-26 18:42:37 +0100

    Levelcontroller Codeoptimierung & Doku
    - bessere Benennung
    - ausführlichere Dokumentation
    - try catch Block für Ajax level Loading
    betrifft #24 und #25

--- source/controller/lab_LevelController.js
+++ source/controller/lab_LevelController.js
@@ -1,25 +1,35 @@
 /**
- * The LevelController takes (1) a level file and (2) one description file for
- * each model and view and translates them to populize the game world
+ * The LevelController takes (1) a level file and (2) a description file for
+ * model and view and translates them to populize the game world
+ * @param gameModel the game model
+ * @param scene3d the 3d render scene
+ * @param scene2d the 2d render scene
  */
-function lab_LevelController(gameModel,scene,minimap){
+function lab_LevelController(gameModel,scene3d,scene2d){
     
+    // the model which holds the game state
     this.gameModel 	= gameModel;
-    this.scene 		= scene;
-    this.minimap 	= minimap;
 
+    // the 3d scene
+    this.scene3d 	= scene3d;
+
+    // the 2d (minimap) scene
+    this.scene2d 	= scene2d;
+
+    // this object can load models
     this.modelLoader 			= new lab_ModelLoader(this.gameModel);
+
+    // this object can load views
     this.representationLoader 	= new lab_RepresentationLoader();
 
     // the 3D objects which have collision according to the model are collected here
     this.collidables = [];
 
-    // the side of the level which is the largest, can be used to define the image section
-    // of the minimap (done in GameController)
-    this.levelSize;
-
+    // the level is rectangular and its size is saved here
     this.levelSizeX;
 	this.levelSizeY;
+	// the side of the level which is the largest
+    this.levelSize;
 }
 
 /**
@@ -27,10 +37,11 @@ function lab_LevelController(gameModel,scene,minimap){
  */
 lab_LevelController.prototype.init = function(){
 	this.initModels();
-	this.initView(this.scene, 'View3D');
-	this.initView(this.minimap, 'View2D');
+	this.initView(this.scene3d, 'View3D');
+	this.initView(this.scene2d, 'View2D');
+
 	// initialize collision detection (only relevant for 3D view)
-	this.initCollidables(this.scene);
+	this.initCollidables(this.scene3d);
 }
 
 /**
@@ -38,15 +49,28 @@ lab_LevelController.prototype.init = function(){
  */
 lab_LevelController.prototype.initModels = function(){
 	// load level data from JSON file
+	try {
+        var level = lab_ajaxGetJson('resources/level-01.json');
+    }
+    catch(e) {
+        throw new Error("Level file could not be loaded! Reason: " + e.message);
+    }
 
-	var level = lab_ajaxGetJson('resources/level-01.json');
-	// get levelsize
+    // each model gets an unique id
+    var id;
+
+	// set levelsize
 	this.levelSizeX = level.levelData[0].line.length;
 	this.levelSizeY = level.levelData.length;
-	
 	this.levelSize = Math.max(this.levelSizeX, this.levelSizeY);
 
+	/*
+	the default size for a representation of an entity is 1, so we set 0.5 as the
+	default value for offset in all directions
+	 */ 
 	var defaultOffset = 0.5;
+
+	// sets the range of the level for position the entities
 	var rangeX = this.levelSizeX/2-defaultOffset;
 	var rangeY = this.levelSizeY/2-defaultOffset;
 
@@ -58,6 +82,7 @@ lab_LevelController.prototype.initModels = function(){
 
 	// ceiling is not defined in level file, set it here
 	id = generateUUID();
+	// the wall height is needed for setting the position of the ceiling
 	var wallHeight = this.representationLoader.representations['wall'].View3D.height;
 	this.gameModel.models[id] = this.modelLoader.createModelByName('ceiling');
 	this.gameModel.models[id].id = id;
@@ -76,8 +101,12 @@ lab_LevelController.prototype.initModels = function(){
 				this.gameModel.models[id].setPosition(j-rangeX,posY,i-rangeY);
 
 				if (this.gameModel.models[id].type == "player") {
-					// create reference for better accessibility
+					// create reference for direct accessibility
 					this.gameModel.player = this.gameModel.models[id];
+					/* 
+					the player is always in the update list, which means its position will be checked
+					permanently in the gameloop
+					*/
 					this.gameModel.addModelToUpdateList(this.gameModel.models[id]);
 				}
 			}
@@ -85,18 +114,19 @@ lab_LevelController.prototype.initModels = function(){
 	}
 }
 
-
-
 /**
- * Loads the corresponding views and adds them to the scene
+ * Loads the corresponding views and adds them to the given scene
+ * @param scene the scene (3d or 2d) which will be populated
+ * @param representationType the type (3d or 2d) as defined in the representation file
  */
 lab_LevelController.prototype.initView = function(scene, representationType){
     // the view elements are collected before drawn in regard to performance
     var viewElements = [];
 	var objectView;
+	var model;
 
     for(var modelId in this.gameModel.models){
-		var model 		= this.gameModel.models[modelId];
+		model = this.gameModel.models[modelId];
 
 		// if floor or ceiling set size dynamically from information about level size
 		if (model.type == 'floor' || model.type == 'ceiling') {
@@ -105,11 +135,12 @@ lab_LevelController.prototype.initView = function(scene, representationType){
 			objectView 	= this.representationLoader.getRepresentation(representationType, model.type);
 		}
 		
-		// only add an object in the scene when it is defined via the representationLoader
+		// only add an object to the scene when it is defined via the representationLoader
 		if (objectView) {
 			// set the id of the view to the same unique id of the model to glue them together
 			objectView.id = modelId;
 
+			// set the position of the view to the models position
 			objectView.position.x = model.getPosition().x;
 			objectView.position.y = model.getPosition().y;
 			objectView.position.z = model.getPosition().z;
@@ -118,14 +149,15 @@ lab_LevelController.prototype.initView = function(scene, representationType){
 		}
 	}
 
-	// add to scene
+	// now all entities are added to the given scene
 	for(i=0;i<viewElements.length;i++) {
 		scene.add(viewElements[i]);
 	}
 }
 
 /**
- * Creates a list with scene objects for collision detection
+ * Checks all models if they are collidable and if so puts them in the collidable list
+ * @param scene the scene which gets collision detection
  */
 lab_LevelController.prototype.initCollidables = function(scene){
 	for (var modelId in this.gameModel.models) {
@@ -137,11 +169,13 @@ lab_LevelController.prototype.initCollidables = function(scene){
 
 /**
  * Updates all views if there are any changes in the model
+ * A update list is used to collect all changed models in one place
  */
 lab_LevelController.prototype.update = function(){
 	for(var modelId in this.gameModel.updateList){
-        this.updateSceneObject(this.scene, modelId);
-        this.updateSceneObject(this.minimap, modelId);
+        this.updateSceneObject(this.scene3d, modelId);
+        this.updateSceneObject(this.scene2d, modelId);
+        // after update remove the model from the update list, but NOT for player
         if (this.gameModel.updateList[modelId].type != 'player') {
         	this.gameModel.removeModelFromUpdateList(this.gameModel.updateList[modelId]);
         }
@@ -150,6 +184,8 @@ lab_LevelController.prototype.update = function(){
 
 /**
  * Updates an object in the view
+ * @param scene the scene (3d or 2d) which will be updated
+ * @param modelId the unique identifier of the model which needs an update
  */
 lab_LevelController.prototype.updateSceneObject = function(scene, modelId){
 	var model		= this.gameModel.updateList[modelId];
@@ -158,10 +194,13 @@ lab_LevelController.prototype.updateSceneObject = function(scene, modelId){
   	if(!view){
     	return false;
 	}
+
+	// update the position of the view to the position of the model
 	view.position.x = model.getPosition().x;
     view.position.y = model.getPosition().y;
     view.position.z = model.getPosition().z;
 
+    // if model is deleted or collected remove if from the scene
     if (this.gameModel.updateList[modelId].isDeleted || this.gameModel.updateList[modelId].isCollected) {
 		scene.remove(view);
 		// delete entity from collision detection list if necessary
@@ -177,17 +216,22 @@ lab_LevelController.prototype.updateSceneObject = function(scene, modelId){
 /**
  * Sets the Y position according to the height definition in the representation loader
  * The position will be set to middle of the height of an object to align to the floor level
+ * @param type the type of the model (e.g. wall)
+ * @param defaultHeight the default height which is used if no height is set
+ * @return the y position of the object
  */
-lab_LevelController.prototype.setObjectYPosition = function(type, defaultOffset){
+lab_LevelController.prototype.setObjectYPosition = function(type, defaultHeight){
+	// height of a 3d object or scale of a sprite is relevant
 	var objectHeight = this.representationLoader.representations[type].View3D.height;
 	if (objectHeight == undefined) {
 		objectHeight = this.representationLoader.representations[type].View3D.scaleY;
 	}
 
 	if (objectHeight != undefined) {
+		// the position is half the height
 		return objectHeight/2;
 	} else {
-		// falls back to default offset if no height is specified
-		return defaultOffset;
+		// fall back to default offset if no height or scale is specified
+		return defaultHeight;
 	}
 }
