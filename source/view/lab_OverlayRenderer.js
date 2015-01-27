1719c9356ce8abd8888a6cfbe5859695336b01d4
hendriksi <hendrik.siebert@mail.de>
2015-01-27 20:41:13 +0100

    Doku overlayRenderer
    Doku overlayRenderer

--- source/view/lab_OverlayRenderer.js
+++ source/view/lab_OverlayRenderer.js
@@ -1,7 +1,14 @@
+/**
+ * The overlay renderer is responsible for showing the HUD elements
+ * @param gameController
+ */
 function lab_OverlayRenderer(gameController){
 	this.gameController = gameController;
 }
 
+/**
+ * initialize all overlay items
+ */
 lab_OverlayRenderer.prototype.render = function() {
 	this.renderHud();
 	this.renderDebugHud();
@@ -9,14 +16,18 @@ lab_OverlayRenderer.prototype.render = function() {
 	this.renderGasMask();
 }
 
+/**
+ * This method handles the gas mask view
+ */
 lab_OverlayRenderer.prototype.renderGasMask = function() {
     var gasmaskOverlay = document.getElementById('gasmaskoverlay');
-
+	// switch limited field of view off
     if(this.gameController.gameModel.player.passiveItem == undefined) {
     	if (gasmaskOverlay.style.display != 'none') {
     		gasmaskOverlay.style.display = 'none';
     	}
     } else {
+		// switch limited field of view on
     	if(this.gameController.gameModel.player.passiveItem.type == 'gasMask'){
 	    	if (gasmaskOverlay.style.display != 'block') {
 	    		gasmaskOverlay.style.display = 'block';
@@ -79,6 +90,9 @@ lab_OverlayRenderer.prototype.renderClock = function () {
     }
 }
 
+/**
+ * This method will be render the HUD inventory and health display 
+ */
 lab_OverlayRenderer.prototype.renderHud = function() {
 	// Load HUD div elements from CSS to var
 	var debugHud = document.getElementById("debug-hud");
@@ -90,16 +104,17 @@ lab_OverlayRenderer.prototype.renderHud = function() {
 	var hudHealthOff = document.getElementById("healthoff");
 	var hudResident = document.getElementById("resident");	
 	
-    var player = gameController.gameModel.player;
+    // get player object from gameController
+	var player = gameController.gameModel.player;
 
-	// Clear HUD items
+	// clear HUD items
 	hudExtinguisher.innerHTML = '<img class="hudimage" src="resources/images/extinguisher_off.png" />';
 	hudAxe.innerHTML = '<img class="hudimage" src="resources/images/axe_off.png" />';
 	hudMedikit.innerHTML = '<img class="hudimage" src="resources/images/medikit_off.png" />';
 	hudGasmask.innerHTML = '<img class="hudimage" src="resources/images/gasmask_off.png" />';
 	hudResident.innerHTML = '<img class="hudimage" src="resources/images/resident_off.png" />';
 	
-	// Show items in inventory on HUD	
+	// show items in inventory on HUD	
     for (var  slot in player.inventory) {
 		if (player.inventory[slot].type == 'axe') hudAxe.innerHTML = '<img class="hudimage" src="resources/images/axe_on.png" />';
 		if (player.inventory[slot].type == 'fireExtinguisher') {
@@ -108,8 +123,8 @@ lab_OverlayRenderer.prototype.renderHud = function() {
 			if(fireExtinguisher) {
   				var uses = fireExtinguisher.amountUses;
 				var size = fireExtinguisher.possibleAmountUses;
+				// calculating filling level of extinguisher to show different images
 				var usesPercent = uses / (size * 0.01);
-				//console.log(usesPercent);
 				if (usesPercent > 66) hudExtinguisher.innerHTML = '<img class="hudimage" src="resources/images/extinguisher_on.png" />';
 				if (usesPercent > 33 && usesPercent < 67) hudExtinguisher.innerHTML = '<img class="hudimage" src="resources/images/extinguisher_on_2-3.png" />';
 				if (usesPercent < 34) hudExtinguisher.innerHTML = '<img class="hudimage" src="resources/images/extinguisher_on_1-3.png" />';
@@ -126,6 +141,7 @@ lab_OverlayRenderer.prototype.renderHud = function() {
 		if (player.activeItem.type == 'fireExtinguisher') {
 			var uses = player.activeItem.amountUses;
 			var size = player.activeItem.possibleAmountUses;
+			// calculating filling level of extinguisher to show different images
 			var usesPercent = uses / (size * 0.01);
 			if (usesPercent > 66) hudExtinguisher.innerHTML = '<img class="hudimage" src="resources/images/extinguisher_active.png" />';
 			if (usesPercent > 33 && usesPercent < 67) hudExtinguisher.innerHTML = '<img class="hudimage" src="resources/images/extinguisher_active_2-3.png" />';	
@@ -137,13 +153,17 @@ lab_OverlayRenderer.prototype.renderHud = function() {
 
 	// Show current health
 	var healthNum = 70 - Math.round(70 * (player.health * 0.01));
+	// resize div for health - only rest health is shown
 	var healthStr = "rect(" + healthNum + "px, 70px, 70px, 0px)";
 	document.getElementById("hud-health-on").style.clip = healthStr;
 	hudHealthOn.innerHTML = '<img class="hudimage" src="resources/images/health_on.png" />';
 	hudHealthOff.innerHTML = '<img class="hudimage" src="resources/images/health_off.png" />';
 }
 
-// Show inventory and health as text - only for debugging
+/**
+ * This method shows the HUD inventory and health as text
+ * Only for debugging
+ */
 lab_OverlayRenderer.prototype.renderDebugHud = function () {
     var text = "";
     var player = gameController.gameModel.player;
