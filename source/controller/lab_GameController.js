4926430bf45897cff8e7fdbd1737cf7207e7ab10
hendriksi <hendrik.siebert@mail.de>
2015-01-27 18:27:42 +0100

    CSS: Divs für HUD umbenannt
    Divs umbenannt wegen Konflikt mit der Soundroutine

--- source/controller/lab_GameController.js
+++ source/controller/lab_GameController.js
@@ -41,7 +41,7 @@ function lab_GameController(screenElement, minimapElement, soundsElement){
 
     this.clock = new THREE.Clock(false);
 
-    this.gameDuration = 20;
+    this.gameDuration = 300;
 
     this.timeLeft = this.gameDuration;
 	
